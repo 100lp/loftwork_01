ebd659d79c62ff6a96504fdef25970978a1db596
100lp <spb.gurme@gmail.com>
2017-02-14 19:45:24 +0300

    Complete 42 tasks (without Proxy task)

--- src/index.js
+++ src/index.js
@@ -27,14 +27,34 @@ function map(array, fn) {
  Напишите аналог встроенного метода reduce для работы с массивами
  */
 function reduce(array, fn, initial) {
+  var previousValue;
+  var i = 0;
+
+  if (initial) {
+    previousValue = initial;
+  } else {
+    previousValue = array[0];
+    i++;
+  }
+
+  for (i; i < array.length; i++) {
+    // тут я застревал так как думал, что не "нормально" передавать во внешнюю функцию арргументы
+    // когда изначально она их не предусматривает и нигде не использует
+    previousValue = fn(previousValue, array[i], i, array);
+  }
+
+  return previousValue;
+
 }
 
 /*
  Задача 4:
- Функция принимает объект и имя свойства, которое необходиом удалить из объекта
+ Функция принимает объект и имя свойства, которое необходимо удалить из объекта
  Функция должна удалить указанное свойство из указанного объекта
  */
 function deleteProperty(obj, prop) {
+  delete obj[prop];
+  return obj;
 }
 
 /*
@@ -43,6 +63,10 @@ function deleteProperty(obj, prop) {
  Функция должна проверить существует ли укзаанное свойство в указанном объекте
  */
 function hasProperty(obj, prop) {
+  if (obj[prop]) {
+    return true;
+  }
+  return false;
 }
 
 /*
@@ -50,6 +74,19 @@ function hasProperty(obj, prop) {
  Функция должна получить все перечисляемые свойства объекта и вернуть их в виде массива
  */
 function getEnumProps(obj) {
+  var result = [];
+  // ОМГ :D
+  // for (var i = 0; i < args.length; i++) {
+  //   for (var prop in obj) {
+  //     if (prop == args[i]) {
+  //       result.push(prop);
+  //     }
+  //   }
+  // }
+  for (var prop in obj) {
+    result.push(prop);
+  }
+  return result;
 }
 
 /*
@@ -57,13 +94,42 @@ function getEnumProps(obj) {
  Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистра и вернуть в виде массива
  */
 function upperProps(obj) {
+  var result = [];
+  for (var prop in obj) {
+    result.push(prop.toUpperCase());
+  }
+  return result;
 }
 
 /*
  Задача 8 *:
  Напишите аналог встроенного метода slice для работы с массивами
  */
-function slice(array, from, to) {
+function slice(array, from = 0, to = array.length) {
+  var result = [];
+
+  if (from < 0) {
+    from = array.length + from;
+    if (from < 0) {
+      from = 0;
+    }
+  }
+
+  if (to < 0) {
+    to = array.length + to;
+    if (to < 0) {
+      to = 0;
+    }
+  }
+
+  if (to > array.length) {
+    to = array.length;
+  }
+
+  for (var i = from; i < to; i++) {
+    result.push(array[i]);
+  }
+  return result;
 }
 
 /*
